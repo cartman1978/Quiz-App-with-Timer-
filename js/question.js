e048e8db8e887e90d7db7dc5583cdfaac18c612c
cartman1978 <cartman_7802@yahoo.ie>
2021-03-01 19:42:02 +0000

    added 5 questions in the object

--- js/question.js
+++ js/question.js
@@ -12,4 +12,49 @@ let question = [
          "Hyper Tool Multi Language"
      ]
     },
+    {
+     numb = 2,
+     question = "What does CSS stand for?",
+     answer = "Hyper Text Markup Language",
+     options: [
+         "Common Style Sheet",
+         "Colorful Style Sheet",
+         "Computer Style Sheet",
+         "Cascading Style Sheet"
+     ]
+    },
+    {
+     numb = 3,
+     question = "What does PHP stand for?",
+     answer = "Hyper Text Markup Language",
+     options: [
+         "Hypertext Processor",
+         "Hypertext Programming",
+         "Hypertext Preprogramming",
+         "Hypertext preprocessor"
+     ]
+    },
+    {
+     numb = 4,
+     question = "What does SQL stand for?",
+     answer = "Hyper Text Markup Language",
+     options: [
+         "Stylish Question Language",
+         "Stylesheet Query Language",
+         "Statement Question Language",
+         "Structured Query Language"
+     ]
+    },
+    {
+     numb = 5,
+     question = "What does XML stand for?",
+     answer = "Hyper Text Markup Language",
+     options: [
+         "eXtensible Markup Language",
+         "eXtensible Multiple Language",
+         "eXtra Multi-Program Language",
+         "eXamine Multiple Language"
+     ]
+    }
+    
 ];
