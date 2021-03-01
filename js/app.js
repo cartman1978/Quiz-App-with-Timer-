207b550d1cb2ed451e0fbd5fecc76b7878a4dfa6
cartman1978 <cartman_7802@yahoo.ie>
2021-03-01 19:17:27 +0000

    added functionality to continue button

--- js/app.js
+++ js/app.js
@@ -3,14 +3,22 @@
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = document.querySelector(".buttons .restart");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
 
 // If start button is clicked
 start_btn.onclick = () => {
-    info_box.classList.add("activeInfo");
+    info_box.classList.add("activeInfo"); //show info box
 }
 
 //If Exit button is clicked
 exit_btn.onclick = () => {
-    info_box.classList.remove("activeInfo");
+    info_box.classList.remove("activeInfo"); //hide info box
 }
+
+//If Continue is clicked
+continue_btn.onclick = () => {
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //Show Quiz
+}
+
