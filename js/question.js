3db5df6f9b91831830f067d685b6d68b25092f2b
cartman1978 <cartman_7802@yahoo.ie>
2021-03-01 19:32:28 +0000

    added firt question object

---
+++ js/question.js
@@ -0,0 +1,15 @@
+// creating an Array and passing numbers, questions, options and answers
+
+let question = [
+    {
+     numb = 1,
+     question = "What does HTML stand for?",
+     answer = "Hyper Text Markup Language",
+     options: [
+         "Hyper Text Processor",
+         "Hyper Text Markup Language",
+         "Hyper Multiple Language",
+         "Hyper Tool Multi Language"
+     ]
+    },
+];
