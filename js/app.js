bb6c719bec636833af2a8a567c7a55b1e7de2ab1
cartman1978 <cartman_7802@yahoo.ie>
2021-03-01 20:28:49 +0000

    added options on the first question array in javascript

--- js/app.js
+++ js/app.js
@@ -20,14 +20,20 @@ exit_btn.onclick = () => {
 continue_btn.onclick = () => {
     info_box.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.add("activeQuiz"); //Show Quiz
-    showQuestions();
+    showQuestions(0);
 }
 
 let que_count = 0;
 
 //getting questions and options from array
-  function showQuestions() {
+  function showQuestions(index) {
       const que_text = document.querySelector(".que_text");
-      let que_tag = '<span>' + questions[0].question + '</span>';
+      const option_list = document.querySelector(".option_list");
+      let que_tag = '<span>' + questions[index].question + '</span>';
+      let option_tag =  '<div class="option">'+ questions[index].options[0] + '<span></span></div>'
+                       +'<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                       + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+                       + '<div class="option">'+ questions[index].options[3] +'<span></span></div>'
       que_text.innerHTML = que_tag;
+      option_list.innerHTML = option_tag;
   }
