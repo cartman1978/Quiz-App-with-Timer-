d521a20e74c00a20a6a619f21409b9be925477bf
cartman1978 <cartman_7802@yahoo.ie>
2021-03-01 14:57:08 +0000

    added required elements in app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// getting all the elements
+
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = document.querySelector(".buttons .restart");
